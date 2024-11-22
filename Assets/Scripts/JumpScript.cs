8ca03aec4bd615c47c978e1c6a58f3f527bad15c
Oskar Rickardsson <oskar.rickardsson12@gmail.com>
2024-11-22 10:27:01 +0100

    add jump script

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+
+public class JumpTest : MonoBehaviour
+{
+    Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerAnimator.SetBool("jumping", false);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            playerAnimator.SetBool("jumping", true);
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = true;
+        }
+        
+        if (Input.GetKey("w"))
+        {
+            playerAnimator.SetBool("jumping", true);
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = false;
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            playerAnimator.SetBool("running", false);
+        }
+        
+        if (Input.GetKeyUp("w"))
+        {
+            playerAnimator.SetBool("running", false);
+        }
+    }
+}
